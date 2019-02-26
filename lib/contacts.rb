06ad13197354bde3f3e5ae52ff2eac92d6a7b320
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-26 00:36:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
 contacts.each do |person_hash, contact_details_hash|
   
   contact_details_hash.each do |details_attribute, details_values|
+    
       if details_attribute == :favorite_ice_cream_flavors
         details_values.delete_if{|ice_cream| ice_cream == "strawberry"}
       end
